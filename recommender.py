3937d906dbfe8d26ef2f4fb2d39509e1edd21a0c
dits <dits@netcompany.com>
2023-11-14 16:26:30 +0100

    Added latest

--- recommender.py
+++ recommender.py
@@ -34,26 +34,53 @@ def pearson(x,y,n):
     # x,y: sets 
     # n: sample size
     x_sum = sum(x)
+    print('xSUM = ',x_sum)
     x_mean = x_sum / len(x)
     y_sum = sum(y)
+    print('ySum = ',y_sum)
     y_mean = y_sum / len(y)
     numerator = 0
     denominator = 0
+    sum1 = 0
+    sum2 = 0
     for i in range(0,n):
         numerator += (x[i] - x_mean) * (y[i] - y_mean)
-        denominator += math.sqrt((x[i] - x_mean) ** 2) * math.sqrt((y[i] - y_mean) ** 2)
+        print(numerator)
+        # denominator += math.sqrt((x[i] - x_mean) ** 2) * math.sqrt((y[i] - y_mean) ** 2)
+        sum1 += math.sqrt((x[i] - x_mean) ** 2)
+        sum2 += math.sqrt((y[i] - y_mean) ** 2)
+
+    denominator = sum1 + sum2
     return numerator / denominator
 
 a = {1, 2, 3, 4}
 b = {3, 4, 5, 6}
 x = [1,2,3,4]
 y = [3,4,5,6]
+ena = [4.0]
+dyo = [3.5,6.0,4.0]
+# x = np.arange(10, 20)
+# y = np.array([2, 1, 4, 5, 8, 12, 18, 25, 96, 48])
 
-print('pearson: ',pearson(x,y,4))
+print('jaccard: ',jaccard(ena,dyo))
 
-def userToUser(y,simFunc,n):
-    # y: set of ratings
+def userToUser(id,simFunc):
+    # id: id of the user for recommendation
     # simFunc: the similarity function used
-    # n: set of users
+    rx = []
+    user_x_ratings = []
+    if ratings_df[['userId']] == id:
+        user_x_ratings.append(ratings_df['rating'])
+
+    metric = 0
+    if simFunc.lower() == "jaccard":
+        metric = jaccard(x,y)
+    elif simFunc.lower() == "dice":
+        metric = dice(x,y)
+    elif simFunc.lower() == "cosine":
+        metric = cosine(x,y)
+    else:
+        metric = pearson(x,y)
+
     for i in range(0,len(n)):
-        
+        print(id)
